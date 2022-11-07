dbb27a57cd7443d51f1c5246b05c839a80f9692f
IvanChudiyovuch <ivanchudiyovuch@ukr.net>
2022-11-07 19:43:55 +0200

    fixed bugs

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
-const MONGO_URL = `mongodb+srv://Vanuha:21011986boss@cluster0.apnxikj.mongodb.net/db-contacts?retryWrites=true&w=majority`;
+const dotenv = require("dotenv");
+dotenv.config();
 
+const { MONGO_URL } = process.env;
 const connectMongo = async () => {
   return mongoose.connect(MONGO_URL);
 };
